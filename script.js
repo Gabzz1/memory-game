b58eaad2af35fafb2fa77eebd0e11acabd9af3a0
Gabzz <sidney.appiah@gmail.com>
2023-01-26 10:07:57 +0000

    created another html page to display 6x6 multiplayer grid

---
+++ script.js
@@ -0,0 +1,24 @@
+/*const controls = document.querySelector(".setupStart-container");
+const stopButton = document.getElementById("controls-container");
+const startButton = document.getElementById("start");
+const body = document.querySelector("body");
+
+
+
+//Start game
+startButton.addEventListener("click", () => {
+    /*movesCount = 0;
+    seconds = 0;
+    minutes = 0;
+    //controls amd buttons visibility
+    controls.classList.add("hide");
+    stopButton.classList.remove("hide");
+    startButton.classList.add("hide");
+    body.classList.add("hide");
+        //Start timer
+    //interval = setInterval(timeGenerator, 1000);
+    //initial moves
+    //moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+    //initializer();
+  });
+  */
